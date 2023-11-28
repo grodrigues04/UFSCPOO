25eed5f41aa43d0c4ee350d937a425b1be25c4a8
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2023-11-27 23:25:41 -0300

    Update 17.py

--- variaveis compostas/17.py
+++ variaveis compostas/17.py
@@ -1,22 +1,22 @@
 #https://www.beecrowd.com.br/repository/UOJ_2227.html
-A,V= [int(x) for x in input().split()] #aeroportos, voos.
+num_aeroportos,num_voos= [int(user_input) for user_input in input().split()] #aeroportos, voos.
 teste = 1
-while A!= 0:
-    frequencia = []
-    dicionario = {}
+while num_aeroportos!= 0:
+    lista_de_frequencia_total = []
+    frequencia_do_aeroporto = {}
     voos = []
-    for c in range(V):
+    for _ in range(num_voos):
         voos.append(tuple([int(x) for x in input().split()]))
-    for d in range(len(voos)):
-        for x in range(2):
-            frequencia.append(voos[d][x])
-    for m in range(1,A+1):
-        dicionario[m] = frequencia.count(m)
-    trafegos = (max(dicionario.values()))
-    chaves_mais_altas = [chave for chave, valor in dicionario.items() if valor == trafegos]
+    for vôo in range(len(voos)):
+        for duplas in range(2):
+            lista_de_frequencia_total.append(voos[vôo][duplas])
+    for aeroporto in range(num_aeroportos):
+        frequencia_do_aeroporto[aeroporto] = lista_de_frequencia_total.count(aeroporto)
+    trafegos = (max(frequencia_do_aeroporto.values()))
+    chaves_mais_altas = [chave for chave, valor in frequencia_do_aeroporto.items() if valor == trafegos]
     print(f" Teste {teste}")
     print(*chaves_mais_altas,end=' ')
     print('')
     teste +=1
-    A,V= [int(x) for x in input().split()]
+    num_aeroportos,num_voos= [int(x) for x in input().split()]
    
