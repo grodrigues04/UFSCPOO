20a71b26bc2697d9525b0b4a3daf61a8cb437e23
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-04-15 16:02:30 -0300

    desafio de floyd

---
+++ floyd/desafio.py
@@ -0,0 +1,36 @@
+from math import log10 
+'''log10 ajuda a calcular quantos digitos de dezena um número tem.
+Assim, consigo determinar quantos zeros preciso colocar na frente dos numeros 
+ '''
+
+linha_atual = 1
+entrada= int(input())
+total = 0
+zero = "0"
+
+while linha_atual < entrada:
+    total = total+linha_atual
+    linha_atual= linha_atual + 1
+
+total = (total+entrada)
+
+linha = 1
+numero = 1
+while linha_atual < entrada:
+    total = total+linha_atual
+    linha_atual=+1
+while linha <= entrada:
+    coluna = 0
+    while coluna < linha:
+        n_de_zero = int(log10(total)) - int(log10(numero))
+        controle = 0
+        ctrZero = ""
+        while controle < n_de_zero:
+            ctrZero = ctrZero + zero
+            controle+=1
+        printador = f'{ctrZero}{numero}'
+        print(printador,end=' ')
+        numero = numero+1
+        coluna = coluna + 1
+    linha = linha + 1
+    print()
